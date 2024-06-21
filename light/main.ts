7553bb1fb59a97d004871061849a33e98d922db2
Niels van de Pas <ik@nielsvandepas.nl>
2024-06-21 10:15:07 +0200

    Add variable work speed

--- light/main.ts
+++ light/main.ts
@@ -12,11 +12,11 @@ loops.everyInterval(100, function () {
     if (SpaceStationX.isPuzzleSolved()) {
         return
     }
-    if (input.lightLevel() >= 196) {
+    for (let index = 0; index < Math.max(0, Math.round(Math.map(input.lightLevel(), 128, 255, 0, 8))); index++) {
         SpaceStationX.workOnPuzzle()
-        SpaceStationX.showWorkGraph(SpaceStationX.Puzzles.Light)
     }
-    if (SpaceStationX.getAmountOfWorkDone() >= 250) {
+    SpaceStationX.showWorkGraph(SpaceStationX.Puzzles.Light)
+    if (SpaceStationX.getAmountOfWorkDone() >= 1600) {
         SpaceStationX.solveLightPuzzle()
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
         SpaceStationX.displayCodeDigit(SpaceStationX.Puzzles.Light)
